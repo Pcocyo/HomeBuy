658586a221dc4f730591f9411edaabc4c50ec163
elson <joshuaelson46@gmail.com>
2024-04-05 07:50:24 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {ChakraProvider} from '@chakra-ui/react'
+import Navigation from './Navigation';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
